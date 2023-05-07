e8634ba25f8fcd4190e3283db44b48d93d3e9b38
jamur <rjamur@gmail.com>
2023-05-07 20:26:29 +0100

    Rows added as list and function created

--- pdfreader.py
+++ pdfreader.py
@@ -3,13 +3,35 @@ import re
 import pandas as pd
 import yfinance as yf
 
-# TODO API yahoo finance
+def addRow(rows, company, ticker):
+	co = yf.Ticker(ticker)
+	history = co.history(period="max", interval="3mo")
+	try:
+		ipo = history.index[0].tz_localize(None) # removed timezone because excel don't support
+		firstValue = history.iloc[0,0]
+		lastValue = history.iloc[-1,0]
+		lastDate = history.index[-1].tz_localize(None)  # removed timezone because excel don't support
+		howMuchPercent = ((lastValue - firstValue) / firstValue) * 100
+	except: # there is no values for ticker
+		ipo = None
+		firstValue = None
+		lastValue = None
+		lastDate = None
+		howMuchPercent = None
+	row = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
+	# add row in the dataframe
+	rows.append(row)
+	print( company + " | " + ticker + " | ̭̭" + str(ipo) + " | ̭̭" + str(firstValue) + " | ̭̭" + str(lastValue) + " | ̭̭" + str(lastDate) + " | ̭̭" + str(howMuchPercent))
+
+	return 
 
 pdf_file = open('ru2000_membershiplist_20220624_0.pdf', 'rb')
 read_pdf = PyPDF2.PdfReader(pdf_file)
 pages = read_pdf.pages
 p = 0
-df = pd.DataFrame(columns = ['Company', 'Ticker', 'IPO', 'First Value', 'Last Value', 'Last Day', 'How Much'])
+columns = ['Company', 'Ticker', 'IPO', 'First Value', 'Last Value',
+	   		 'Last Date', 'How Much']
+rows = []
 
 # Lê até encontrar as strings que indicam final
 while True:
@@ -33,53 +55,29 @@ while True:
 				break
 			company = splited[c]
 			ticker = splited[c+1]
-			co = yf.Ticker(ticker)
-			history = co.history(period="max")
-			try:
-				ipo = history.index[0]
-				firstValue = history.iloc[0,0]
-				lastValue = history.iloc[-1,0]
-				lastDate = history.index[-1]
-				howMuchPercent = ((lastValue - firstValue) / firstValue) * 100
-			except:
-				ipo = "*******"
-				firstValue = "*******"
-				lastValue = "*******"
-				lastDate = "*******"
-				howMuchPercent = "*******"
-
-			df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
-			print( company + " | " + ticker)
+			addRow(rows, company, ticker)
+			#df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
 			c += 2
+			df = pd.DataFrame(rows, columns = columns)
+			print(df)
+			df.to_excel('SuperTempShares.xlsx')
+
+
 	else:
 		# Na página 10 o PDF fica sem quebra de linhas entre colunas
-		# cabeçalhos
+		
 		c = 0
+		# cabeçalhos
 		headers = splited[c].rsplit(maxsplit=1)
 		print(headers[0] + " | " + headers[1])
 		c += 1
 		while splited[c][:13] != "June 24, 2022":
+			# separa o ticker, que está a direita
 			subsplited = splited[c].rsplit(maxsplit=1)
 			company = subsplited[0]
 			ticker = subsplited[1]
-			co = yf.Ticker(ticker)
-			history = co.history(period="max")
-			try:
-				ipo = history.index[0]
-				firstValue = history.iloc[0,0]
-				lastValue = history.iloc[-1,0]
-				lastDate = history.index[-1]
-				howMuchPercent = ((lastValue - firstValue) / firstValue) * 100
-			except:
-				ipo = "*******"
-				firstValue = "*******"
-				lastValue = "*******"
-				lastDate = "*******"
-				howMuchPercent = "*******"
-
-			# add row in the dataframe
-			df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
-			print( company + " | " + ticker)
+			addRow(rows, ticker)
+			#df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
 			c += 1
 
 	if ended:
@@ -87,9 +85,12 @@ while True:
 		break
 
 	p += 1
+	df = pd.DataFrame(rows, columns = columns)
+	
+	#print(df)
+	#df.to_excel('TempShares.xlsx')
 
 print(df)
-
 df.to_excel('Shares.xlsx')
 
 #print("Sem eliminar as quebras")
@@ -102,3 +103,7 @@ df.to_excel('Shares.xlsx')
 #print("Pegando apenas as 20 primeiras posições")
 #novastring = parsed[0:20]
 #print(novastring)
+
+
+
+
