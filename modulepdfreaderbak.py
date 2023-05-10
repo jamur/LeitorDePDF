036dd9646f9367766ba97c1f5b8ee96e3cb9dca2
jamur <rjamur@gmail.com>
2023-05-10 20:38:18 +0100

    cleaned

--- modulepdfreaderbak.py
+++ modulepdfreaderbak.py
@@ -3,15 +3,21 @@ import re
 import pandas as pd
 import yfinance as yf
 from datetime import datetime, timedelta
+#import logging
 import concurrent.futures
+import threading
 import time
 from progressbar import progressbar
 from alive_progress import alive_bar
+
+# TODO Threads
 
 class PDFReader():
 
 	def __init__(self, pdfFile):
 		format = "%(asctime)s: %(message)s"
+		#logging.basicConfig(format=format, level=logging.INFO,
+		#					datefmt="%H:%M:%S")
 		pdf = open(pdfFile, 'rb')
 		read_pdf = PyPDF2.PdfReader(pdf)
 		self.pages = read_pdf.pages
@@ -19,25 +25,31 @@ class PDFReader():
 	def addCompany(self, companies, company, ticker):
 		row = [company, ticker]
 		companies.append(row)
+		#logging.info( company + " | " + ticker )
 		return 
 	
-	# Read PDF and return companies names and tickers
+	# Lê o PDF e retorna uma lista de listas: [Nome da Empresa, Ticker]
 	def readCompanies(self) -> list:
 		companies = []
 		pageNumber = 0
-		# Read until find end strings
+		# Lê até encontrar as strings que indicam final
 		while True:
 			page = self.pages[pageNumber]
 			page_content = page.extract_text()
 			parsed = ''.join(page_content)
 			cels = parsed.split('\n')
+			#print("\n\n---------------------")
+			#print("Página " + str(pageNumber+1))
+			#print("---------------------\n")
 			celNumber = 0
 			cab = (cels[celNumber])
-			if cab == "Company": # pageNumber != 9: # There are break lines among
+			if cab == "Company": # pageNumber != 9: # Tratamento normal - a não ser a página 10 (no else) que está fora do padrão
+				#cabs
 				celNumber += 2
 				ended = False
-				while cels[celNumber] != "June 24, 2022": # Pages end string
-					if cels[celNumber] == "ftserussell.com": # End string of all
+				while cels[celNumber] != "June 24, 2022": #String do fim de cada página
+					if cels[celNumber] == "ftserussell.com": #String no final de toda a lista
+						#print("----------------\nTerminou\n----------------")
 						ended = True
 						break
 					company = cels[celNumber]
@@ -45,33 +57,57 @@ class PDFReader():
 					if company != "Company": # test if cab
 						self.addCompany(companies, company, ticker)
 
+					#df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
 					celNumber += 2
-			else: # cab = "Company Ticker" - Method to discover other pattern of page
-				# No break lines in page 10
-				# cabs
+					#df = pd.DataFrame(self.rows, columns = columns)
+					#print(df)
+					#df.to_excel('SuperTempShares.xlsx')
+			else: # if cab = "Company Ticker" - Nem method to discover other pattern of page
+				# Na página 10 o PDF fica sem quebra de linhas entre colunas
+				# cabeçalhos
 				headers = cels[celNumber].rsplit(maxsplit=1)
+				#print(headers[0] + " | " + headers[1])
 				celNumber += 1
 				while cels[celNumber][:13] != "June 24, 2022":
-					# split ticker at right
+					# separa o ticker, que está a direita
 					subCel = cels[celNumber].rsplit(maxsplit=1)
 					company = subCel[0]
 					ticker = subCel[1]
 					if company != "Company": # test if cab
 						self.addCompany(companies, company, ticker)
+					#df.loc[len(df.index)] = [company, ticker, ipo, firstValue, lastValue, lastDate, howMuchPercent]
 					celNumber += 1
 
 			if ended:
+				#print("----------------\nTerminou Mesmo\n----------------")
 				break
 
 			pageNumber += 1
+			#df = pd.DataFrame(rows, columns = columns)
+			
+			#print(df)
+			#df.to_excel('TempShares.xlsx')
 
 		self.companies = companies
 		return companies
 	
+	def getFirstValue(self, co, ipo):
+		limit = 1
+		day = ipo
+		while limit < 180:
+			history = co.history(start=day, end=day + timedelta(1))
+			firstValue = history.iloc[0,0]
+			if firstValue:
+				return firsValue
+			day += timedelta(1)
+			limit += 1
+
 	def addYFRow(self, companyRow):
 		company = companyRow[0]
 		ticker = companyRow[1]
+		#print(ticker, end=".")
 		co = yf.Ticker(ticker)
+		#history = co.history(period="max", interval="3mo")
 		try: # Ticker ONEM raises an error, for ex.
 			info = co.info
 			firstUTC = info['firstTradeDateEpochUtc']
@@ -82,20 +118,29 @@ class PDFReader():
 			return
 
 		ipo = datetime.utcfromtimestamp(firstUTC).date()
+		# ipo = history.index[0].date() # .tz_localize(None).date() # removed timezone because excel don't support
 		history = co.history(start=ipo, end=ipo + timedelta(1))
 
 		try:
 			firstValue = history.iloc[0,0]
 		except:
-			# no history data for ticker
 			firstValue = None
+
+		#if firstValue == 0:
+		#	firstValue = self.getFirstValue(co, ipo)
 
 		try:
 			lastValue = co.info['currentPrice'] #history.iloc[-1,0]
 		except:
-			# no data
+			#history = co.history(period = "1d")
 			lastValue = None
 
+		#history = co.history(period="1m")
+		#try:
+		#	lastDate = history.index[-1].date()  # removed timezone because excel don't support
+		#	lastDate = lastDate.strftime("%d/%m/%Y")
+		#except:
+		#	lastDate = None
 		lastDate = None
 
 		if firstValue and lastValue:
@@ -120,6 +165,48 @@ class PDFReader():
 		# add row in the list
 		return row
 
+	# based on self.
+	def readYFDataRowsThreadBak(self,qt=0) -> list:
+		self.yfDataRows = []
+		threads = list()
+		#logging.info("Runing threads")
+		if not(qt): # if no qt then qt = total
+			qt = len(self.companies)
+		print("Creating Threads...")
+		with alive_bar(qt) as bar:
+			for companie_number in range(qt):
+				companie = self.companies[companie_number]
+				#print(".", end="")
+				#logging.info(f"Main    : before creating thread {t}")
+				x = threading.Thread(target=self.addYFRow, args=(companie,))
+				threads.append(x)
+				#logging.info(f"Main    : before running thread {t}")
+				x.start()
+				#logging.info(f"Main    : wait for the thread to finish {t}")
+				#x.join()
+				bar()
+
+		threads_qt = len(threads)
+		print("Waiting Threads...")
+		with alive_bar(qt) as bar:
+			while threads_qt > 0:
+				time.sleep(5)
+				for thread in threads:
+					if not(thread.is_alive()):
+						threads_qt -= 1
+						bar()
+
+		# Só pra garantir
+		for thread in threads:
+			thread.join()
+
+		#logging.info("Main    : all done")
+		#with concurrent.futures.ThreadPoolExecutor as executor:
+		#	executor.map(self.addYFRow, companyTupleOfTuples)
+		#for company in companies:
+		#	self.addYFRow(yfDataRows, company)
+		return self.yfDataRows
+
 	def readYFDataRowsThread(self,qt=0) -> list:
 		self.yfDataRows = []
 		if qt:
@@ -127,6 +214,7 @@ class PDFReader():
 		else:
 			companies = self.companies
 		threads = list()
+		#logging.info("Runing threads")
 		if not(qt): # if no qt then qt = total
 			qt = len(self.companies)
 		with concurrent.futures.ThreadPoolExecutor() as exe:
@@ -135,28 +223,45 @@ class PDFReader():
 			futures = [exe.submit(self.addYFRow, companie) for companie in companies]
 			print("Executing Threads...")
 			with alive_bar(qt) as bar:
-	        	# update bar as tasks are completed
+	        	# handle results as tasks are completed
 				for future in concurrent.futures.as_completed(futures):
+					#print(f'>got {future.result}')
 					bar()
 
+			#with alive_bar(qt) as bar:
+			#	# issue one task for each call to the function
+			#	for result in exe.map(self.addYFRow, companies):
+			#		#print(f'>got {result}')
+			#		bar()
+
+
     	# report that all tasks are completed
-		print('Done.')
+		print('Done')
 		return self.yfDataRows
 
 
 	def readYFDataRowsNoThread(self, qt=0) -> list:
 		self.yfDataRows = []
+		#logging.info(self.companies)
 		if not(qt): # if no qt then qt = total
 			qt = len(self.companies)
 		with alive_bar(qt) as bar:
 			for companie_number in range(qt):
 				companie = self.companies[companie_number]
 				self.addYFRow(companie)
+				#print(".", end="")
+				#logging.info(companie[1])
 				bar()
+		#logging.info("Main    : all done")
+		#with concurrent.futures.ThreadPoolExecutor as executor:
+		#	executor.map(self.addYFRow, companyTupleOfTuples)
+		#for company in companies:
+		#	self.addYFRow(yfDataRows, company)
 		return self.yfDataRows
 
 	def readYFDataRows(self, thread=False, qt=0):
 		self.yfDataRows = []
+		#self.readYFDataRowsThread()
 		if thread:
 			self.readYFDataRowsThread(qt)
 		else:
@@ -165,9 +270,11 @@ class PDFReader():
 	   		 'Last Date', 'How Much']
 		self.df = pd.DataFrame(self.yfDataRows, columns = columns)
 		return self.yfDataRows
+		
 
 	def saveExcel(self, arqName):
 		self.df.to_excel(arqName)
+
 
 if __name__ == "__main__":
 	qt =  0
@@ -180,4 +287,26 @@ if __name__ == "__main__":
 	execution_time_str = time.strftime("%H:%M:%S", time.gmtime(execution_time))
 	print(f"The execution time is: {execution_time_str}")
 	pdf_reader.saveExcel('CompaniesGrowing.xlsx')
+
+	# More than half more time
+	#start_time = time.perf_counter()
+	#df_yf_data = pdf_reader.readYFDataRows(qt, thread=False)
+	#end_time = time.perf_counter()
+	#execution_time = end_time - start_time
+	#print(f"The execution time with no thread is: {execution_time}")
+	#pdf_reader.saveExcel('NoThread.xlsx')
+
+#print("Sem eliminar as quebras")
+#print(parsed)
+
+#parsed = re.sub('n', '', parsed)
+#print("Após eliminar as quebras")
+#print(parsed)
+
+#print("Pegando apenas as 20 primeiras posições")
+#novastring = parsed[0:20]
+#print(novastring)
+
+
+
 
